38d24e9ab5b5e98fbb16246304411d624024dfd9
Heixier <rzsiah@gmail.com>
2025-04-18 16:21:15 +0800

    fixed push_swap in wrong directory; added philo min_eat checker

---
+++ projects/philo/philo_eat.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+LIGHT_BLUE=$'\033[38;5;75;1m'
+RED=$'\033[38;5;160;1m'
+ORANGE=$'\033[38;5;208;1m'
+RESET=$'\033[0m'
+
+PROGRAM_NAME="philo"
+
+check_prog_exist () {
+	if ! [ -f "$PROGRAM_NAME" ]; then
+		printf "%serror: \"philo\" not found!\n%s" "$RED" "$RESET"
+		exit 127
+	fi
+}
+
+main () {
+	check_prog_exist
+
+	printf "%sRunning program...%s" "$ORANGE" "$RESET"
+	result=("$(./"$PROGRAM_NAME" "$@")")
+	printf "\r"; tput el
+	local died="$(printf "%s\n" "${result[@]}" | grep "died")"
+
+	if [[ "$died" ]]; then
+		printf "%sTest invalid: %s%s\n" "$RED" "$died" "$RESET"
+		return 1
+	fi
+	min_eat="$(printf "%s\n" "${result[@]}" | grep "is eating" | awk '{ print $2 }' | sort | uniq -c | sort -n | awk 'NR==1 {print $1}')"
+	printf "%sMinimum times eaten: %s%s%s%s\n" "$ORANGE" "$LIGHT_BLUE" "$min_eat" "$RESET"
+}
+
+main "$@"
+
+
+
